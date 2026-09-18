fc35bde33d4912c4fa647ce8986937f02bad5f4e
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-18 22:43:34 +0900

    섹션4. String 클래스- 주요 메서드 문자열 정보 조회

--- src/lang/String/method/StringInfoMain.java
+++ src/lang/String/method/StringInfoMain.java
@@ -4,14 +4,14 @@ public class StringInfoMain {
     static void main(String[] args) {
 
         String str = "Hello, Java!";
-        System.out.println("문자열의 길이: " + str.length());
-        System.out.println("문자열이 비어 있는지: " + str.isEmpty());
-        System.out.println("문자열이 공백인지: " + str.isBlank());
-        System.out.println("문자열이 비어 있거나 공백인지 1: " + str.isBlank());
-        System.out.println("문자열이 비어 있거나 공백인지 2: " + "            ".isBlank());
+        System.out.println("문자열의 길이: " + str.length());                 //문자열의 길이: 12
+        System.out.println("문자열이 비어 있는지: " + str.isEmpty());            //문자열이 비어 있는지: false
+        System.out.println("문자열이 공백인지: " + str.isBlank());              //문자열이 공백인지: false
+        System.out.println("문자열이 비어 있거나 공백인지 1: " + str.isBlank());     //문자열이 비어 있거나 공백인지 1: false
+        System.out.println("문자열이 비어 있거나 공백인지 2: " + "            ".isBlank());      //문자열이 비어 있거나 공백인지 2: true
 
         char c = str.charAt(7);
-        System.out.println("7번인덱스의 문자 = " + c);
+        System.out.println("7번인덱스의 문자 = " + c);     //7번인덱스의 문자 = J
 
     }
 }
