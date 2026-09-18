f656d8b4880bfec1fa98cd6fa976ae70074e8d6e
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-18 21:09:38 +0900

    섹션4. String 클래스- 주요 메서드1 문자열 검색

--- src/lang/String/method/StringSearchMain.java
+++ src/lang/String/method/StringSearchMain.java
@@ -9,5 +9,6 @@ public class StringSearchMain {
         System.out.println("'Java'의 첫 번째 인덱스: " + str.indexOf("Java"));
         System.out.println("인덱스 10부터 'Java'의 인덱스: " + str.indexOf("Java",10));
         System.out.println("'Java'의 마지막 인덱스: " + str.lastIndexOf("Java"));
+
     }
 }
